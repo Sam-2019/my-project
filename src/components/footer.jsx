2726dd0c40d5cea0473dc53c64e469bd09a68eb2
Samuel Martey <sam@encodevlabs.com>
2023-03-31 20:28:25 +0000

    fixed broken footer links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,13 +6,13 @@ export default function Footer() {
   <footer className="bg-white">
    <div className="mx-auto max-w-7xl overflow-hidden py-5 px-6 sm:py-5 lg:px-8">
     <nav
-     className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+     className="-mb-6 text-center sm:flex sm:justify-center sm:space-x-12"
      aria-label="Footer"
     >
      {navigation.map((item) => (
       <div key={item.name} className="pb-6">
        <a
-        href={item.href}
+        href={item.path}
         className="text-sm leading-6 text-gray-600 hover:text-gray-900"
        >
         {item.name}
@@ -33,7 +33,8 @@ export default function Footer() {
      ))}
     </div> */}
     <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-     &copy; {new Date().getFullYear()} Chriscomik Company, Limited. All rights reserved.
+     &copy; {new Date().getFullYear()} Chriscomik Company, Limited. All rights
+     reserved.
     </p>
    </div>
   </footer>
