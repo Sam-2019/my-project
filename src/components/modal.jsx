5acf5455395699db7c0743e9dc1ee88452fbf108
Samuel Martey <sam@encodevlabs.com>
2023-04-02 13:09:01 +0000

    added image description

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -51,11 +51,11 @@ export default function MyModal({ show, setIsOpen, data, setData }) {
           <div class="overflow-hidden cursor-pointer rounded-xl relative group">
            <div class="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
             <div>
-             {/* <div class="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
-              <div class="font-bold">{data.alt}</div>
+             <div class="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
+              {/* <div class="font-bold">{data.alt}</div> */}
 
-              <div class="opacity-60 text-sm ">{data.alt}</div>
-             </div> */}
+              <div class="opacity-60 text-sm ">{data.description}</div>
+             </div>
             </div>
            </div>
            <img
